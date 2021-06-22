e1e1b77517acce5215af45f65cd38f9b6b128b44
tassadar777 <nnk592@gmali.com>
2021-06-22 21:27:46 +0300

    add platform support

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 gpio-wakeup
 
-This is an experimental driver to enable a wakeup button on ODROID N2 SBC running a 5.x kernel.
+This is an experimental driver to enable a GPIO wakeup button on ODROID N2 SBC running a 5.x kernel.
 For now the pin value is hardcoded into the source file (gpiopower=479, which corresponds to pin 11). 
 To set it to a different value, find your signal pin export number from this table: https://wiki.odroid.com/odroid-n2/hardware/expansion_connectors.
+
+First, you need to add the lines from dts.txt to your source dtb file, rebuild the database, install it to your boot partition and restart the computer.
+Then you should cd to the module source directory, build and insmod.
 
 To build the kernel module:
  $ make
--- gpio-wakeup.c
+++ gpio-wakeup.c
@@ -1,4 +1,11 @@
-/* 
+/*
+ * GPIO wakeup driver for ODROID N2
+ *
+ * Copyright (C) 2021 Andrew Ivanchuk
+ *
+ * This file was based on: drivers/amlogic/input/keyboard/gpio_keypad.c
+ * 	Copyright (C) 2017 Amlogic, Inc. All rights reserved
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -16,51 +23,67 @@
 #include <linux/platform_device.h>
 #include <linux/gpio.h>
 
-MODULE_LICENSE("GPL");
-MODULE_INFO(intree, "Y");
+#define DRV_NAME        "gpio-wakeup"
 
 /*The value is taken from drivers/amlogic/input/keyboard/gpio_keypad.c of 4.9.x Amlogic kernel*/
 #define AO_DEBUG_REG0           ((0x28 << 2))
 
 static unsigned long gpiopower=479;
 
-static int gpio_wakeup_init(void)
+static int gpio_wakeup_probe(struct platform_device *pdev)
 {
         void __iomem *ao_reg=NULL;
-	/*struct resource *res;*/
+	struct resource *res;
 	int val;
 
-	printk(KERN_INFO "[gpio-wakeup] test driver initializing...\n");
+	dev_info(&pdev->dev,"test driver initializing...\n");
 	
 	/* Values are taken from gpio_keypad section of meson64_odroidn2.dts 
-	 * file from Hardkernel's 4.9.x kernel: */
-
-	/* Does not work for now
-	res=request_mem_region(0xFF800000,0x400,"gpio-wakeup");
+	 * file from Hardkernel's 4.9.x kernel: 0xFF800000,0x400 */
+	
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	if(res)	
 		ao_reg=ioremap(res->start,res->end - res->start);
 	else
-		printk(KERN_ERR "[gpio-wakeup] cannot obtain i/o mem region\n");
-	*/
+		dev_err(&pdev->dev,"cannot obtain i/o mem region\n");
 	
-	ao_reg=ioremap(0xFF800000,0x400);
-
 	if (ao_reg) {
 		val = readl((ao_reg + AO_DEBUG_REG0));
 		val |= (gpiopower << 16);
 		writel(val, (ao_reg + AO_DEBUG_REG0));
-		printk(KERN_INFO "[gpio-wakeup] successfully set up power button #%ld for wakeup at 0x%x\n",
-				gpiopower,0xFF800000+AO_DEBUG_REG0);
+		dev_info(&pdev->dev,"successfully set up power button #%ld for wakeup at 0x%lx\n",
+				gpiopower,(unsigned long) res->start+AO_DEBUG_REG0);
 		iounmap(ao_reg);
 	}
 
 	return 0;
 }
-static void gpio_wakeup_exit(void)
+static int gpio_wakeup_remove(struct platform_device *pdev)
 {
-	printk(KERN_INFO "[gpio-wakeup] test driver unloaded\n");
+	dev_info(&pdev->dev, "test driver unloaded\n");
+	return 0;
 }
 
-module_init(gpio_wakeup_init);
-module_exit(gpio_wakeup_exit);
+static const struct of_device_id gpio_wakeup_of_match[] = {
+        { .compatible = "amlogic,gpio-wakeup", },
+        { },
+};
+MODULE_DEVICE_TABLE(of, gpio_wakeup_of_match);
+
+static struct platform_driver gpio_wakeup_driver = {
+        .probe  = gpio_wakeup_probe,
+	.remove = gpio_wakeup_remove,
+        .driver = {
+                .name   = DRV_NAME,
+                .of_match_table = gpio_wakeup_of_match,
+        },
+};
+
+module_platform_driver(gpio_wakeup_driver);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("GPIO wakeup driver for ODROID N2");
+MODULE_ALIAS("platform:" DRV_NAME);
+MODULE_INFO(intree, "Y");
+
 
